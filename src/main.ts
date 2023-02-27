6939fac36a3921248b715a71105b6c46f999cbaa
iamgamja <hmmthinking@naver.com>
2023-02-27 17:04:25 +0900

    add createjs lib, add canvas demo

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,22 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import { setupCounter } from './counter'
+import createjs from 'createjs-module'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const app = document.querySelector('#app')
+if (!(app instanceof HTMLDivElement)) throw new Error('No App')
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const canvas = document.createElement('canvas')
+canvas.setAttribute('id', 'main')
+canvas.setAttribute('width', '500')
+canvas.setAttribute('height', '300')
+
+app.appendChild(canvas)
+
+const stage = new createjs.Stage('main')
+
+const circle = new createjs.Shape()
+circle.graphics.beginFill('DeepSkyBlue').drawCircle(0, 0, 50)
+circle.x = 100
+circle.y = 100
+stage.addChild(circle)
+
+stage.update()
